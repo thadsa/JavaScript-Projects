66cba01d97948c59d7daa11adc0658dce258819f
thadsa <94719073+thadsa@users.noreply.github.com>
2021-12-19 20:13:45 -0500

    places

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,23 @@
+function myfav() {
+    var sentence = "My favourite";
+    sentence +=" Place is Srilanka!!";
+    document.getElementById("favo").innerHTML = sentence;
+}
+function myFuncation(){
+    var sentence = "Srilanka is";
+    sentence += " my favourite Places!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+    var sentence = "India is";
+    sentence += " The most populous country!";
+    document.getElementById("Concatenate1").innerHTML = sentence;
+    var sentence = "UK is";
+    sentence += " Great Britain and Northern Ireland!";
+    document.getElementById("Concatenate2").innerHTML = sentence;
+    var sentence = "US is";
+    sentence += " US is a country primarily located on North America!";
+    document.getElementById("Concatenate3").innerHTML = sentence;
+    var sentence = "Canada is";
+    sentence += " second -largest country in the world, after Russia!";
+    document.getElementById("Concatenate4").innerHTML = sentence;
+}
+/* From this assignement I finish like insweting += operator and displaying funtion*/
