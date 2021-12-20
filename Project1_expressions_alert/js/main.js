ca9b387f7cca002ad69d0ed7bf7375bba000fe5a
thadsa <94719073+thadsa@users.noreply.github.com>
2021-12-20 12:33:55 -0500

    alert

--- Project1_expressions_alert/js/main.js
+++ Project1_expressions_alert/js/main.js
@@ -5,6 +5,6 @@ function My_Best_Function() {
     document.getElementById("Color_Text").innerHTML=result
 }
 function My_last_Function(){
-    document.write("Welcome to the my JavaScript site!!!")= str.fontcolor("Green");
+    document.write("Thanks for visiting my site!!!")= str.fontcolor("red");
    /* So basically I add windows.alert(), document.write(), color in JS */
 }
