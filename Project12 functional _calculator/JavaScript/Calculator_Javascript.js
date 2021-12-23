88de3f2a1dedb5b067f1a2f67af870f9306edcee
thadsa <94719073+thadsa@users.noreply.github.com>
2021-12-23 13:01:13 -0500

    calculator

--- Project12 functional _calculator/JavaScript/Calculator_Javascript.js
+++ Project12 functional _calculator/JavaScript/Calculator_Javascript.js
@@ -1,91 +1,99 @@
-const Calculator = {
-    Display_Value: '0',
-    First_Operand: null,
-    Wait_Second_Operand: false,
-    Operator: null,
-};
+ const Calculator = { 
+    Display_Value: '0', 
+    First_Operand: null, 
+     Wait_second_operand: false,  
+    operator: null,
+    };
 
-function Input_Digit(digit) {
-    const { Display_Value, Wait_Second_Operand } = Calculator;
-    if (Wait_Second_Operand === true) {
-        Calculator.Display_Value = digit;
-        Calculator.Wait_Second_Operand = false;
-    } else {
-        Calculator.Display_Value = Display_Value === '0' ? digit : Display_Value + digit;
+    
+    function Input_Digit (digit) {
+        const { Display_Value, Wait_Second_Operand } = Calculator; 
+        if (Wait_Second_Operand === true) {
+            Calculator.Display_Value = digit;
+            Calculator.Wait_Second_Operand = false; 
+        } else { 
+         Calculator.Display_Value = Display_Value === '0' ? digit : Display_Value + digit;
+        }
     }
-}
-
-function Input_Decimal(dot) {
-    if (Calculator.Wait_Second_Operand === true) return;
-    if (!Calculator.Display_Value.includes(dot)) {
-        Calculator.Display_Value += dot;
+        
+        function Input_Decimal(dot) { 
+        if (Calculator.Wait_Second_Operand === true) return; 
+        if (!Calculator.Display_Value.includes(dot)) { 
+        Calculator. Display_Value += dot;
+        }
     }
-}
-
-function Handle_Operator(Next_Operator) {
-    const {First_Operand, Display_Value, Operator } = Calculator
-    const Value_of_Input = parseFloat(Display_Value);
-    if (Operator && Calculator.Wait_Second_Operand) {
-        Calculator.Operator = Next_Operator;
-        return;
+           
+     function Handle_Operator (Next_Operator) {
+        const { First_Operand, Display_Value, operator } = Calculator 
+        const Value_of_Input = parseFloat(Display_Value);  
+        if (operator && Calculator.Wait_Second_Operand) {
+            Calculator.operator = Next_Operator; 
+            return;
+        }
+        if (First_Operand == null) {
+            Calculator.First_Operand = Value_of_Input; 
+        } else if (operator) { 
+            const Value_Now = First_Operand || 0; 
+            const result = Perform_Calculation [operator] (Value_Now, Value_of_Input);
+            
+            Calculator.Display_Value = String(result); 
+            Calculator.First_Operand = result;
+        }
+            
+            Calculator.Wait_Second_Operand = true; 
+            Calculator.operator = Next_Operator;
     }
-    if (First_Operand === null) {
-        Calculator.First_Operand = Value_of_Input;
-    } else if (Operator) {
-        const Value_Now = First_Operand || 0;
-        let result = Perform_Calculation[Operator](Value_Now, Value_of_Input);
-        result = Number(result).toFixed(9)
-        result = (result * 1).toString()
-        Calculator.Display_Value = parseFloat(result);
-        Calculator.First_Operand = parseFloat(result); 
+    
+    const Perform_Calculation = {
+        '/': (First_Operand, Second_Operand) => First_Operand / Second_Operand,
+    
+        '*': (First_Operand, Second_Operand) => First_Operand * Second_Operand,
+    
+        '+': (First_Operand, Second_Operand) => First_Operand + Second_Operand,
+    
+        '-': (First_Operand, Second_Operand) => First_Operand - Second_Operand,
+    
+        '=': (First_Operand, Second_Operand) => Second_Operand
+    };
+        
+    function Calculator_Reset() {
+        Calculator.Display_Value = '0'; 
+        Calculator.First_Operand = null; 
+        Calculator.Wait_Second_Operand = false; 
+        Calculator.operator = null;
     }
-    Calculator.Wait_Second_Operand = true;
-    Calculator.Operator = Next_Operator;
-}
-
-const Perform_Calculation = {
-    '/': (First_Operand, Second_Operand) => First_Operand / Second_Operand,
-    '*': (First_Operand, Second_Operand) => First_Operand * Second_Operand,
-    '+': (First_Operand, Second_Operand) => First_Operand + Second_Operand,
-    '-': (First_Operand, Second_Operand) => First_Operand - Second_Operand,
-    '=': (First_Operand, Second_Operand) => Second_Operand,
-};
-
-function Calculator_Reset () {
-    Calculator.Display_Value = '0';
-    Calculator.First_Operand = null;
-    Calculator.Wait_Second_Operand = false;
-    Calculator.Operator = null;
-}
-
-function Update_Display() {
-    const display = document.querySelector('.calculator-screen');
-    display.value = Calculator.Display_Value;
-}
-
-Update_Display();
-const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', (event) => {
-    const { target } = event;
-    if (!target.matches('button')) {
-        return;
-    }
-
-    if (target.classList.contains('operator')) {
-        Handle_Operator(target.value);
-        Update_Display();
-        return;
-    }
-    if (target.classList.contains('decimal')) {
-        Input_Decimal(target.value);
-        Update_Display();
-        return;
-    }
-    if (target.classList.contains('all-clear')) {
-        Calculator_Reset();
-        Update_Display();
-        return;
-    }
-    Input_Digit(target.value);
-    Update_Display();
-})
+        
+        function Update_Display() {
+            const display = document.querySelector('.calculator-screen'); 
+            display.value = Calculator.Display_Value;
+        }
+     
+        Update_Display(); 
+        const keys = document.querySelector('.calculator-keys'); 
+        keys.addEventListener('click', (event) => {
+            
+            const { target } = event; 
+            if (!target.matches('button')) {
+            return;
+            }
+            if (target.classList.contains('operator')) {
+              Handle_Operator (target.value); 
+              Update_Display(); 
+              return;
+            }
+    
+            if (target.classList.contains('decimal')) { 
+            Input_Decimal(target.value); 
+            Update_Display();
+              return;
+            }
+            
+            if (target.classList.contains ('all-clear')) {
+              Calculator_Reset(); 
+              Update_Display(); 
+              return;
+            }
+    
+            Input_Digit(target.value); 
+            Update_Display();
+        }) 
