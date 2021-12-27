72c17028bc2fb6becca0614b44f8ea0765498236
thadsa <94719073+thadsa@users.noreply.github.com>
2021-12-26 22:47:51 -0500

    Ajax basic project

--- Project1_expressions_alert/js/main.js
+++ Project1_expressions_alert/js/main.js
@@ -1,13 +1,9 @@
-function My_Best_Function() {
-    window.alert("Click ok to get the answer");
-    var str = "My favourite color is Black!";
-    var result = str.fontcolor("black")
-    document.getElementById("Color_Text").innerHTML=result
-}
-function My_last_Function(){
-    /*document.write("Volvo!!!");
-    var str ="My car name is?"
-    var carname ="Volvo"*/
-    document.getElementById("Car-name").innerHTML = carName;
-   /* So basically I add windows.alert(), document.write(), color in JS */
-}
+const a = 225; // number
+const b = true; // boolean
+
+//converting to string
+const result1 = String(a);
+const result2 = String(b);
+
+console.log(result1); // "225"
+console.log(result2); // "true"
